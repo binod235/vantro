ba6b41990866ad6c417657941a883cd04140184a
binod235 <poudelbinod552@gmail.com>
2026-06-14 18:17:19 +0100

    Fix trustedOrigins for production

--- api/src/modules/auth/auth.config.ts
+++ api/src/modules/auth/auth.config.ts
@@ -64,8 +64,10 @@ export const auth = betterAuth({
   },
 
   trustedOrigins: [
-    process.env.BETTER_AUTH_URL ?? 'http://localhost:3000',
-    process.env.FRONTEND_URL ?? 'http://localhost:3001',
+    process.env.BETTER_AUTH_URL  ?? 'http://localhost:3000',
+    process.env.FRONTEND_URL     ?? 'http://localhost:3001',
+    process.env.FRONTEND_URL_WWW ?? 'http://localhost:3001',
+    'http://localhost:3001',
   ],
 });
 
